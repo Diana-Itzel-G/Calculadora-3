bbee57f0fc7fc3aa8797938832c7cc326178652a
Diana <gonzalezvasquezdianaitzel@gmail.com>
2025-03-10 19:40:55 -0600

    aaaaaaaaaaaahhhhhhhh

--- calculadora biblioteca/Class1.cs
+++ calculadora biblioteca/Class1.cs
@@ -49,6 +49,7 @@ namespace  calculadora_biblioteca
                 throw new DivideByZeroException("El denominador no puede ser cero.");
             return numerador / denominador;
         }
-        // rafa el agrega otra operacion porfavor 
+        // operacion rafa
+
     }
 }  
